f5851a5547ac8c3b803d801e90e231737a2e9a74
Heli Vepsäläinen <L4136@student.jamk.fi>
2018-01-28 20:28:56 +0200

    staattiset metodit done.

---
+++ ItParser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestParser
+{
+    class ItParser
+    {
+        //int Count(string syote);
+        //int Sum(string syote);
+        //int Average(string syote);
+
+        public static int Count(string syote)
+        {
+            int count = 1;
+            foreach (char x in syote)
+            {
+                if (x == ',') count++;
+            }
+            return count;
+        }
+
+        public static int Sum(string syote)
+        {
+            int sum = 0;
+            int strInt = 0;
+            List<string> stringNum = new List<string>();
+            string charString = "";
+
+            foreach (char y in syote)
+            {
+                //add char to temp string
+                if (y != ',')
+                {
+                    charString += y;
+                }
+                //add temp string to list and clear temp string
+                if (y == ',')
+                {
+                    stringNum.Add(charString);
+                    charString = "";
+                }
+            }
+
+            //list strings to int, sum ints
+            stringNum.Add(charString);
+            foreach (string e in stringNum)
+            {
+                strInt = int.Parse(e);
+                sum += strInt;
+                strInt = 0;
+            }
+            return sum;
+        }
+
+        public static int Average(string syote)
+        {
+            int elements = Count(syote);
+            int sum = Sum(syote);
+            int average = sum / elements;
+            return average;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@ namespace TestParser
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Anna haluamasi lukujono merkkijonona, luvut pilkulla eroteltuina: ");
+            string syote = Console.ReadLine();
+            int count = ItParser.Count(syote);
+            int sum = ItParser.Sum(syote);
+            int average = ItParser.Average(syote);
+            Console.WriteLine("Merkkijonossa " + syote +" on " + count + " lukua. Lukujen summa on " + sum +" ja keskiarvo on " + average +".");
         }
     }
 }
